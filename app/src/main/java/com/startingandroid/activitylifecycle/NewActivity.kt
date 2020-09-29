28e40b170a0702b6d557ba9810719e02278d25fb
SAMcric <sam1024yak@gmail.com>
2020-09-30 01:23:38 +0530

    new change

--- app/src/main/java/com/startingandroid/activitylifecycle/NewActivity.kt
+++ app/src/main/java/com/startingandroid/activitylifecycle/NewActivity.kt
@@ -7,5 +7,6 @@ class NewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new)
+        //new change
     }
 }
