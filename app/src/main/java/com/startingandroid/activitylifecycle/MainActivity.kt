cb31e1e929a47a0d2fb81b0086349c52ec2c4c62
SAMcric <sam1024yak@gmail.com>
2020-09-30 01:16:24 +0530

    Initial commit

---
+++ app/src/main/java/com/startingandroid/activitylifecycle/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.startingandroid.activitylifecycle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.scrollview_example)
+        println("onCreate called")
+        title ="Demo Project"
+    }
+
+    override fun onStart() {
+        super.onStart()
+        println("onStart called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        println("onResume called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        println("onPause called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        println("onPuased called")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        println("onRestart called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        println("onDestroy called")
+    }
+
+
+}
